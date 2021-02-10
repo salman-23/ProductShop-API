dab9432c3dead124f63e212f506302cbdea5f9c3
salman-23 <66154732+salman-23@users.noreply.github.com>
2021-02-10 15:10:34 +0300

    refactoring using middleware

--- app.js
+++ app.js
@@ -5,9 +5,22 @@ const app = express();
 app.use(express.json());
 app.use("/products", productRoutes);
 
+// placement is improtent
+// next() to terimaite the middleware
 
+//Not Found Middleware
+app.use((req, res, next) => {
+  next({ status: 404, message: "Path Not Found" });
+});
 
-const PORT = 8000;
+// Eror Handeling Middleware
+app.use((err, req, res, next) => {
+  res
+    .status(err.status ? err.status : 500)
+    .json({ message: err.message ? err.message : "Internal Server Error" });
+});
+
+const PORT = 8001;
 app.listen(PORT, () => {
   console.log(`Running on ${PORT}`);
 });
--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,17 +1,17 @@
 const { Product } = require("../db/models");
 
 //Product Create
-exports.productCreate = async (req, res) => {
+exports.productCreate = async (req, res, next) => {
   try {
     const newProduct = await Product.create(req.body);
     res.status(201).json(newProduct);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
 
 //Product Update
-exports.productUpdate = async (req, res) => {
+exports.productUpdate = async (req, res, next) => {
   const { productId } = req.params;
   try {
     const foundProduct = await Product.findByPk(productId);
@@ -19,30 +19,34 @@ exports.productUpdate = async (req, res) => {
       await foundProduct.update(req.body);
       res.status(204).end();
     } else {
-      res.status(404).json({ message: "Sorry Product Not Found!" });
+      err = {
+        status: 404,
+        message: "Sorry Product Not Found!",
+      };
+      next(err);
     }
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
 
 //Product List
 
-exports.productList = async (req, res) => {
+exports.productList = async (req, res, next) => {
   try {
     const _products = await Product.findAll({
       attributes: { exclude: ["createdAt", "updatedAt"] },
     });
     res.json(_products);
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
 
 //Product Delete
 
-exports.productDelete = async (req, res) => {
+exports.productDelete = async (req, res, next) => {
   const { productId } = req.params;
   try {
     const foundProduct = await Product.findByPk(productId);
@@ -50,9 +54,13 @@ exports.productDelete = async (req, res) => {
       await foundProduct.destroy();
       res.status(204).end();
     } else {
-      res.status(404).json({ message: "Sorry Product Not Found!" });
+      err = {
+        status: 404,
+        message: "Sorry Product Not Found!",
+      };
+      next(err);
     }
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
