1da87801a8f081a905aca8f178dc00d2a95559f4
salman-23 <66154732+salman-23@users.noreply.github.com>
2021-02-11 14:32:23 +0300

    add cors

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const productRoutes = require("./routes/products");
 const app = express();
 
+// placement is improtent
 app.use(express.json());
+app.use(cors());
 app.use("/products", productRoutes);
 
-// placement is improtent
-// next() to terimaite the middleware
-
 //Not Found Middleware
+// next() to terimaite the middleware
 app.use((req, res, next) => {
   next({ status: 404, message: "Path Not Found" });
 });
